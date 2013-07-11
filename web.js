224dcebd7f7ab10a51dc985b0515ab515b1fd18c
karthikkv81 <karthikkvenkatesan@gmail.com>
2013-07-11 23:39:11 +0000

    modified to read web.js

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html', function(err, data){
-  if (err) throw err;
+  
+  var data = fs.readFileSync('index.html');
+  
   response.send(data);
-  }
+  
 });
 
 var port = process.env.PORT || 5000;
