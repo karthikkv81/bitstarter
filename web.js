1d138e697464f74625d29b59b83f138a828463c3
karthikkv81 <karthikkvenkatesan@gmail.com>
2013-07-11 23:43:31 +0000

    modified to read web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   
-  var data = fs.readFileSync('index.html');
+  var data = fs.readFileSync('index.html',"utf8");
   
   response.send(data);
   
